b898e97a2ef0543ba820ca601e49147d79e46bac
Stormy-nahlui <52010138+Stormy-nahlui@users.noreply.github.com>
2019-07-08 09:22:06 -0700

    Create React App Generated
    basic create react app + material ui

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+
+const drawerWidth = 240;
+const useStyles = makeStyles(theme=>({
+  root: {
+    flexGrow: 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+}));
+
+function App() {
+
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  function handleDrawerOpen() {
+    setOpen(true);
+  }
+
+  function handleDrawerClose() {
+    setOpen(false);
+  }
+
+  const classes = useStyles();
+
+  return (
+    <div className="App">
+      <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} onClick={handleDrawerOpen} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Team Silent
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+      </Drawer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
